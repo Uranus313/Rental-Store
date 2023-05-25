cb7bf74f3ee159a562433746c6a37a522655bcf7
Uranus313 <mehrbodmh82@gmail.com>
2023-05-25 10:51:37 +0330

    implementing JSON

--- demo/src/main/java/Ure/IntertainmentRental.java
+++ demo/src/main/java/Ure/IntertainmentRental.java
@@ -1,3 +1,5 @@
+package Ure;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -5,15 +7,15 @@ class Item{
     protected String title;
     protected String genre;
     protected String releaseDate;
-    protected String ID;
+    protected int id;
     protected double rentalFee;
-    protected boolean isAvailable = true;
-    public Item(String title, String genre, String releaseDate, String iD, double rentalFee) {
+    protected boolean isAvailable ;
+    public Item(String title, String genre, String releaseDate, int id, boolean isAvailable) {
         this.title = title;
         this.genre = genre;
         this.releaseDate = releaseDate;
-        ID = iD;
-        this.rentalFee = rentalFee;
+        this.id = id;
+        this.isAvailable = isAvailable;
     }
     public String getTitle() {
         return title;
@@ -24,8 +26,8 @@ class Item{
     public String getReleaseDate() {
         return releaseDate;
     }
-    public String getID() {
-        return ID;
+    public int getid() {
+        return id;
     }
     public double getRentalFee() {
         return rentalFee;
@@ -39,73 +41,104 @@ class Item{
     public void setRentalFee(double rentalFee) {
         this.rentalFee = rentalFee;
     }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
     
     
 }
 class Movie extends Item{
-    private ArrayList<String> cast;
-    private String Director;
-    public Movie(String title, String genre, ArrayList<String> cast, String releaseDate, String ID,double rentalFee,String Director) {
-        super(title, genre, releaseDate, ID, rentalFee);
-        this.cast = new ArrayList<>(cast);
-        this.Director = Director;
+    private String cast;
+    private String director;
+    public Movie(String title, String genre, String cast, String releaseDate, int id,boolean isAvailable,String director) {
+        super(title, genre, releaseDate, id, isAvailable);
+        this.cast = cast;
+        this.director = director;
     }
-    public ArrayList<String>  getCast() {
+    public String  getCast() {
         return cast;
     }
-    public String getDirector() {
-        return Director;
+    public String getdirector() {
+        return director;
+    }
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
 class Game extends Item{
-    private String platform;
-    private String publisher;
+    private String manufacturer;
+    private String director;
     
-    public Game(String title, String genre,String Platform , String releaseDate, String ID,double rentalFee,String Publisher) {
-        super(title, genre, releaseDate, ID, rentalFee);
-        this.platform = Platform;
-        this.publisher = Publisher;
+    public Game(String title, String genre,String manufacturer , String releaseDate, int id,boolean isAvailable,String director) {
+        super(title, genre, releaseDate, id, isAvailable);
+        this.manufacturer = manufacturer;
+        this.director = director;
     }
-    public String getID() {
-        return ID;
+    public int getid() {
+        return id;
     }
-    public String getPlatform() {
-        return platform;
+    public String getmanufacturer() {
+        return manufacturer;
     }
-    public String getPublisher() {
-        return publisher;
+    public String getdirector() {
+        return director;
+    }
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+    public void setDirector(String director) {
+        this.director = director;
     }
 }
 class Book extends Item{
-    private String author;
+    private String writer;
     private String publisher;
     
-    public Book(String title, String genre,String author , String releaseDate, String ID,double rentalFee,String Publisher) {
-        super(title, genre, releaseDate, ID, rentalFee);
-        this.author = author;
+    public Book(String title, String genre,String writer , String releaseDate, int id,boolean isAvailable,String Publisher) {
+        super(title, genre, releaseDate, id, isAvailable);
+        this.writer = writer;
         this.publisher = Publisher;
     }
-    public String getAuthor() {
-        return author;
+    public String getwriter() {
+        return writer;
     }
     public String getPublisher() {
         return publisher;
+    }
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
     }
 }
 class Customer{
     private String name;
     private String email;
-    private int phone;
+    private String phone;
     private String address;
-    private String ID;
+    private int id;
     private ArrayList<Rental> rentals;
-    public Customer(String name, String email, int phone, String address, String id) {
+    public Customer(String name, String email, String phone, String address, int id,ArrayList<Rental> rentals) {
         this.name = name;
         this.email = email;
         this.phone = phone;
         this.address = address;
-        this.ID = id;
-        rentals = new ArrayList<>();
+        this.id = id;
+        this.rentals = new ArrayList<>();
+
+    }
+    public void initialize(){
+        this.rentals = new ArrayList<>();
     }
     public String getName() {
         return name;
@@ -113,29 +146,45 @@ class Customer{
     public String getEmail() {
         return email;
     }
-    public int getPhone() {
+    public String getPhone() {
         return phone;
     }
     public String getAddress() {
         return address;
     }
-    public String getId() {
-        return ID;
+    public int getid() {
+        return id;
     }
     public ArrayList<Rental> getRentals() {
         return rentals;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setRentals(ArrayList<Rental> rentals) {
+        this.rentals = rentals;
+    }
+    
 }
 class Rental{
     private Item item;
-    private Customer customer;
-    private String ID;
+    private int customerid;
+    private int id;
     private Date rentalDate;
     private Date returnDate;
-    public Rental(Item item, Customer customer, String iD) {
+    public Rental(Item item, Customer customer, int i) {
         this.item = item;
-        this.customer = customer;
-        ID = iD;
+        this.customerid = customer.getid();
+        id = i;
         item.setAvailable(false);
         this.rentalDate = new Date();
         customer.getRentals().add(this);
@@ -143,11 +192,11 @@ class Rental{
     public Item getItem() {
         return item;
     }
-    public Customer getCustomer() {
-        return customer;
+    public int getCustomerid() {
+        return customerid;
     }
-    public String getID() {
-        return ID;
+    public int getid() {
+        return id;
     }
     public Date getRentalDate() {
         return rentalDate;
@@ -193,10 +242,12 @@ class RentalStore{
         return AvailableItems;
     }
     public void rentItem(Item item,Customer customer){
+        System.out.println(1);
         for(Item i: items){
         if(i.equals(item)){
         if(i.isAvailable()){
-            new Rental(item, customer, customer.getId() + item.getID());
+            System.out.println(8);
+            new Rental(item, customer, customer.getid() + item.getid());
         }
     }        
     }
@@ -204,17 +255,17 @@ class RentalStore{
     public void returnItem(Rental rental){
         rental.setReturnDate(new Date());
     }
-    public Customer getCustomerByID(String id){
+    public Customer getCustomerByid(int id){
         for(int i =0;i<customers.size();i++){
-            if(customers.get(i).getId().equals(id)){
+            if(customers.get(i).getid()==id){
                 return customers.get(i);
             }
         }
         return null;
     }
-    public Item getItemByID(String id){
+    public Item getItemByid(int id){
         for(int i =0;i<items.size();i++){
-            if(items.get(i).getID().equals(id)){
+            if(items.get(i).getid()==id){
                 return items.get(i);
             }
         }
